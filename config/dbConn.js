8f210ae0a8bf4f558ecd5060451aa63e93176e97
Apus Industries <folorunsopraise580@gmail.com>
2023-06-17 03:11:58 +0100

    added db port

--- config/dbConn.js
+++ config/dbConn.js
@@ -4,6 +4,7 @@ const mySql = require( 'mysql' )
 const pool = mySql.createPool( {
       connectionLimit: 10,
       host: 'sql306.infinityfree.com',
+      port: 3306,
       user: 'epiz_30088273',
       password:process.env.DB_PASSWORD,
       database: 'epiz_30088273_delvet'
