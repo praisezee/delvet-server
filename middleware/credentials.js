c1c97ad0b17c2eefc2b62b676056c4cac9890a2f
Folorunso praise Oluwapelumi <106927972+praisezee@users.noreply.github.com>
2023-06-17 02:44:03 +0100

    Update credentials.js

--- middleware/credentials.js
+++ middleware/credentials.js
@@ -5,7 +5,6 @@ const credentials = ( req, res, next ) =>
       const origin = req.headers.origin;
       if ( allowedOrigins.includes( origin ) ) {
             res.header( 'Access-Control-Allow-Credentials', true )
-            res.header('Access-Control-Allow-Origin', '*')
       }
       next()
 };
