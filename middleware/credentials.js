33781039da82564e98e18c4ac951ed587eb3bc44
Folorunso praise Oluwapelumi <106927972+praisezee@users.noreply.github.com>
2023-06-17 02:53:29 +0100

    Update credentials.js

--- middleware/credentials.js
+++ middleware/credentials.js
@@ -5,6 +5,9 @@ const credentials = ( req, res, next ) =>
       const origin = req.headers.origin;
       if ( origin  ) {
             res.header( 'Access-Control-Allow-Credentials', true )
+            res.header('Access-Control-Allow-Origin', 'https://delvet.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
       }
       next()
 };
